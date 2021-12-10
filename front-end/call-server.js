feb995dc5fd5ec14527eb248cbd3cb6ce824fd07
agushini <56412460+agushini@users.noreply.github.com>
2021-12-09 20:04:19 -0500

    logging out for the night

--- front-end/call-server.js
+++ front-end/call-server.js
@@ -1,8 +1,11 @@
-var fullDeck = Array.from(Array(4), () => new Array(13));    //deck to reinitalize deck if we run out of elements
-var playingDeck = Array.from(Array(4), () => new Array(13)); //deck we will deal from
+let fullDeck = Array.from(Array(4), () => new Array(13));    //deck to reinitalize deck if we run out of elements
+let playingDeck = Array.from(Array(4), () => new Array(13)); //deck we will deal from
+let playerTurn;
+let playerTotal = 0; //this is the total for now until someone implements the database login function
+let dealerTotal = 0;
 
 function start() {
-    //need to check if ef is null because otherwise we get an error of trying to assign to null variable
+
     let loginButton = document.querySelector('#btn_login');
     let startButton = document.querySelector('#btn_start');
     
@@ -10,6 +13,8 @@ function start() {
     let hitButton = document.querySelector('#btn_hit');
     let stayButton = document.querySelector('#btn_stay');
     let dealButton = document.querySelector('#btn_deal');
+    
+    //need to check if ef is null because otherwise we get an error of trying to assign to null variable
     if(loginButton)
         loginButton.addEventListener('click', handleLogin);
     if(startButton)
@@ -29,7 +34,7 @@ function start() {
 }
 
 
-function randomNum(a){ //returns 0-a randomly, no decimal and is inclusive therefore chooses the turns when 0-1
+function randomNum(a){ //returns 0-a randomly, no decimal and is inclusive
     let min=0;
     let max=a;
     min = Math.ceil(min);
@@ -40,17 +45,22 @@ function randomNum(a){ //returns 0-a randomly, no decimal and is inclusive there
 function dealCard(){
     var y = randomNum(playingDeck.length-1); 
     var x = randomNum(playingDeck[y].length-1);
+    var output = 0;
+    /* Testing purposes
     console.log(" y length : " + playingDeck.length + " x length : " + playingDeck[y].length);
     console.log("Array indexes for the card  y,x: " + y + " " + x);
     console.log(playingDeck[y][x])
     console.log('full deck at location: ' + fullDeck[y][x])
-    playingDeck[y][x]; //this will be the card that is dealt
+    */
+    output = playingDeck[y][x]; //this will be the card that is dealt
     playingDeck[y].splice(x,1);//remove that card from the deck
     
     console.log(" y length : " + playingDeck.length + " x length : " + playingDeck[y].length);
     
+    return output;
 
     //testing the deck thats what most of this console stuff is
+    /*
     var temp =  playingDeck.length;
     console.log("output deck after splice: ")
     for(var i = 0; i < temp; i++){
@@ -59,8 +69,8 @@ function dealCard(){
             console.log(playingDeck[i][j]);
         }
         console.log("newline");
-    }
-}
+    }*/
+}//end dealCard()
 
 function copyDeck(){
     for(var i = 0; i < 4; i++){
@@ -70,7 +80,7 @@ function copyDeck(){
             x++
         }
     }
-}
+}//end copyDeck()
 
 function createDeck(){
     //fullDeck = [[1,2,3,4,5,6,7,8,9,10,11,12,13]     //clubs 1=ACE 11=Jack 12=Queen 13=King
@@ -88,13 +98,15 @@ function createDeck(){
 
     copyDeck();
 
+    //for testing purposes
+    /*
     for (let i = 0; i < 4; i++){
         for (let j = 0; j < 13; j++){
             console.log(fullDeck[i][j])
         }
         console.log("\n");
-    }
-}
+    }*/
+}//end copyDeck
 
 function handleLogin(){ // we need to do something with this
     console.log("Login button clicked");
@@ -114,16 +126,18 @@ function handleLogout(){
 
 function handleHit(){ // we need to do something with this
     console.log("Hit button clicked");
+    playerTotal = playerTotal + dealCard();
 }
 
-function handleStay(){ // we need to do something with this
+function handleStay(){ 
+    //stay should just switch the turns to the dealer
     console.log("Stay button clicked");
-
 }
 
-function handleDeal(){ // we need to do something with this
+function handleDeal(){ 
     console.log("Deal button clicked");
     dealCard();
+
 }
 
 
