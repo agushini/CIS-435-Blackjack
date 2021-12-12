71097a4b2724c943af5014c30fd6d5ffce6e22ca
epuschel <73982403+epuschel@users.noreply.github.com>
2021-12-12 01:31:07 -0500

    Incorrect password functionality now works

--- front-end/login.php
+++ front-end/login.php
@@ -2,14 +2,14 @@
  require_once('database.php');
  $username = $_POST["username"];
  $pa55word = $_POST["pa55word"];
- 
+
 //Check if the username already exists within the database
 $query1 = 'SELECT * FROM blackjack
            WHERE username = :username';
 $statement1 = $db->prepare($query1);
 $statement1->bindValue(':username', $username);
 $statement1->execute();
-//$product1 = $statement1->fetch();
+$product1 = $statement1->fetch();
 $statement1->closeCursor();
 //If the username does not exist in the database, insert the username and pa55word with 0 wins into the database.
 //FIXME: Need to check if the pa55word is valid.
@@ -38,9 +38,10 @@ header("Location: game.html");
     $statement3 = $db->prepare($query3);
     $statement3->bindValue(':username', $username);
     $statement3->bindValue(':pa55word', $pa55word);
-    $product3 = $statement3->setFetchMode(PDO::FETCH_ASSOC);
-    
+    $statement3->execute();
+    $product3 = $statement3->fetch();
     $statement3->closeCursor();
+    //echo implode(" ", $product3);
     if (!$product3) {
         echo "Login failed. Please enter correct username and password.";
     } else {
@@ -50,11 +51,7 @@ header("Location: game.html");
     //echo "Is this happening?";
     //echo $product3;
     //If the username and pa55word are correct
-    /*if (!$product3) {
-        
-    } else {
-        
-    }*/
+
     
 }
 ?>
